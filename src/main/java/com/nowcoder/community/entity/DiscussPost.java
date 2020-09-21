baa1189830f09544f1a9ac41b0767bced1d38b9e
seven_asus_book <517455715@qq.com>
2020-09-21 21:18:52 +0800

    Spring 整合 ES, 导入依赖, 在需要用ES的实体类上加入对应注释. 对需要搜索的属性进行特别标注, 添加 搜索存储与 分词约束. 测试基本的ES 存储, 搜索, 删除, 查询功能

--- src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,17 +1,43 @@
 package com.nowcoder.community.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+
 import java.util.Date;
 
+//ES：indexName 【索引】,type 【类型】,shards 【分片】,replicas 【副本】
+@Document(indexName = "discusspost",type = "_doc",shards = 6,replicas = 3)
 public class DiscussPost {
 
+    @Id
     private int id;
+
+    @Field(type = FieldType.Integer)
     private int userId;
+
+    //analyzer 【存储解析器】,searchAnalyzer = 【搜索解析器】
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String title;
+
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String content;
+
+    @Field(type = FieldType.Integer)
     private int type;
+
+    @Field(type = FieldType.Integer)
     private int status;
+
+    @Field(type = FieldType.Date)
     private Date createTime;
+
+    @Field(type = FieldType.Integer)
     private int commentCount;
+
+    @Field(type = FieldType.Double)
     private double score;
 
     public int getId() {
